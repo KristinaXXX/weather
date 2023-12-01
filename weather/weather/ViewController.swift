122bb3130911a83582458014240c93aa50fe3783
KristinaXXX <107407324+KristinaXXX@users.noreply.github.com>
2023-12-01 10:25:55 +0500

    initial commit

---
+++ weather/weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by Kr Qqq on 01.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
